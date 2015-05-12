5b89ad51800687ac71aed199c08a2e38f82dd38d
Greg Woods <greg@westlakedesign.com>
2015-05-12 12:47:53 -0400

    add support for currency with 0 precision

--- src/format-currency.js
+++ src/format-currency.js
@@ -1,16 +1,16 @@
 function numberToCurrency(number, options) {
     // set defaults...
     if (typeof options == 'undefined') { options = {}; }
-    var precision = options.precision || 2
-    var unit      = options.unit      || '$'
-    var separator = options.separator || '.'
-    var delimiter = options.delimiter || ','
-    var format    = options.format    || '%u%n'
-    var negativeFormat = options.negativeFormat || '-%u%n'
+    var precision = typeof(options.precision) == 'number' ? options.precision : 2;
+    var unit      = options.unit      || '$';
+    var separator = options.separator || '.';
+    var delimiter = options.delimiter || ',';
+    var format    = options.format    || '%u%n';
+    var negativeFormat = options.negativeFormat || '-%u%n';
 
     // "clean up" number
     if (typeof number == 'string') { number = number.replace(/\$/g, ''); } // strip dollar sign
-    number = isNaN(number) || number == '' || number == null ? 0.0 : number; // set to 0.0 if we can't tell what it is
+    number = isNaN(number) || number === '' || number === null ? 0.0 : number; // set to 0.0 if we can't tell what it is
 
     // determine which format to use
     if (number < 0) { 
@@ -20,9 +20,16 @@ function numberToCurrency(number, options) {
 
     // 'separate' the cents
     var numberStr = parseFloat(number).toFixed(precision).toString();
-    var numberFormatted = new Array(numberStr.slice(-1*precision));   // this returns the cents
-    numberFormatted.unshift(separator); // add the separator
-    numberStr = numberStr.substring(0, numberStr.length-(precision+1)); // this removes the decimal and cents
+    var numberFormatted;
+
+    if(precision > 0){
+        numberFormatted = new Array(numberStr.slice(-1*precision));   // this returns the cents
+        numberFormatted.unshift(separator); // add the separator
+        numberStr = numberStr.substring(0, numberStr.length-(precision+1)); // this removes the decimal and cents
+    }
+    else{
+        numberFormatted = [];
+    }
 
     // 'delimit' the thousands
     while (numberStr.length > 3) {
